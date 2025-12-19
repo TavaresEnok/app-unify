0216cc0dec82a84aba73c4d0b96cd83626775f33
Developer <dev@unified.app>
2025-12-19 18:23:44 +0000

    chore: Remove layouts 12, 13, 14, 15 as requested

--- lib/layout_selector.dart
+++ lib/layout_selector.dart
@@ -17,22 +17,6 @@ import 'layouts/layout_10/pages/speed_test_page.dart' as l10_speed;
 import 'layouts/layout_11/dashboard_page.dart' as l11_dashboard;
 import 'layouts/layout_11/login_page.dart' as l11_login;
 
-// Imports Layout 12 (Minimalist)
-import 'layouts/layout_12/dashboard_page.dart' as l12_dashboard;
-import 'layouts/layout_12/login_page.dart' as l12_login;
-
-// Imports Layout 13 (Futuristic)
-import 'layouts/layout_13/dashboard_page.dart' as l13_dashboard;
-import 'layouts/layout_13/login_page.dart' as l13_login;
-
-// Imports Layout 14 (Premium Dark)
-import 'layouts/layout_14/dashboard_page.dart' as l14_dashboard;
-import 'layouts/layout_14/login_page.dart' as l14_login;
-
-// Imports Layout 15 (Dynamic)
-import 'layouts/layout_15/dashboard_page.dart' as l15_dashboard;
-import 'layouts/layout_15/login_page.dart' as l15_login;
-
 // Imports dos Logins (diferentes por layout)
 import 'layouts/layout_02/login_page.dart' as l02_login;
 import 'layouts/layout_03/login_page.dart' as l03_login;
@@ -251,74 +235,6 @@ class LayoutSelector {
           onRefresh: onRefresh,
         );
 
-      case 'layout_12':
-        return l12_dashboard.ProviderDashboardPage(
-          customerName: customerName,
-          planName: planName,
-          connectionStatus: connectionStatus,
-          billAmount: billAmount,
-          billDueDate: billDueDate,
-          usedGb: usedGb,
-          totalGb: totalGb,
-          downloadMbps: downloadMbps,
-          uploadMbps: uploadMbps,
-          onNavigate: onNavigate,
-          menuItems: menuItems,
-          customCardBg: customCardBg,
-          customCardText: customCardText,
-          invoiceColor: invoiceColor,
-          actionColor: actionColor,
-          onRefresh: onRefresh,
-        );
-
-      case 'layout_13':
-        return l13_dashboard.DashboardPage(
-          customerName: customerName,
-          planName: planName,
-          connectionStatus: connectionStatus,
-          billAmount: billAmount,
-          billDueDate: billDueDate,
-          usedGb: usedGb,
-          totalGb: totalGb,
-          downloadMbps: downloadMbps,
-          uploadMbps: uploadMbps,
-          onNavigate: onNavigate,
-          menuItems: menuItems,
-          onRefresh: onRefresh,
-        );
-
-      case 'layout_14':
-        return l14_dashboard.ProviderDashboardPage(
-          customerName: customerName,
-          planName: planName,
-          connectionStatus: connectionStatus,
-          billAmount: billAmount,
-          billDueDate: billDueDate,
-          usedGb: usedGb,
-          totalGb: totalGb,
-          downloadMbps: downloadMbps,
-          uploadMbps: uploadMbps,
-          onNavigate: onNavigate,
-          menuItems: menuItems,
-          onRefresh: onRefresh,
-        );
-
-      case 'layout_15':
-        return l15_dashboard.DashboardPage(
-          customerName: customerName,
-          planName: planName,
-          connectionStatus: connectionStatus,
-          billAmount: billAmount,
-          billDueDate: billDueDate,
-          usedGb: usedGb,
-          totalGb: totalGb,
-          downloadMbps: downloadMbps,
-          uploadMbps: uploadMbps,
-          onNavigate: onNavigate,
-          menuItems: menuItems,
-          onRefresh: onRefresh,
-        );
-
       default:
         // Default to Layout 02 if unknown
         return l02.ProviderDashboardPage(
@@ -363,14 +279,6 @@ class LayoutSelector {
         return const l10_login.LoginPage();
       case 'layout_11':
         return const l11_login.LoginPage();
-      case 'layout_12':
-        return const l12_login.LoginPage();
-      case 'layout_13':
-        return const l13_login.LoginPage();
-      case 'layout_14':
-        return const l14_login.LoginPage();
-      case 'layout_15':
-        return const l15_login.LoginPage();
 
       default:
         return const l02_login.LoginPage();
@@ -514,26 +422,6 @@ class LayoutSelector {
       'id': 'layout_11',
       'name': 'Cyberpunk / Neon',
       'description': 'Tema futurista escuro com detalhes em neon Cyan e Pink'
-    },
-    {
-      'id': 'layout_12',
-      'name': 'Minimalist Typography',
-      'description': 'Foco total em tipografia grande e espaço negativo'
-    },
-    {
-      'id': 'layout_13',
-      'name': 'Futuristic Cyber',
-      'description': 'Estilo industrial clean com holografia e neon'
-    },
-    {
-      'id': 'layout_14',
-      'name': 'Premium Gold Dark',
-      'description': 'Luxo, texturas escuras e acentos dourados'
-    },
-    {
-      'id': 'layout_15',
-      'name': 'Dynamic Modular',
-      'description': 'Grid modular vibrante e interativo'
     },
   ];
 }
